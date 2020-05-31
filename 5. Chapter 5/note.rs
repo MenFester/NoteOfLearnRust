660cfc0edf86333c20778edae56db8b8425759e9
MenFester <13500177552@qq.com>
2020-05-31 17:25:36 +0800

    2020年05月31日 17:25

--- 5. Chapter 5/note.rs
+++ 5. Chapter 5/note.rs
@@ -55,6 +55,53 @@ impl T {
     }
 }
 
+trait Double {
+    fn double(&self) -> Self;
+}
+
+impl Double for i32 {
+    fn double(&self) -> i32 {
+        *self * 2
+    }
+}
+
+trait Cook {
+    fn start(&self);
+}
+
+trait Wash {
+    fn start(&self);
+}
+
+struct Chef;
+
+impl Cook for Chef {
+    fn start(&self) {
+        println!("Cook::start");
+    }
+}
+
+impl Wash for Chef {
+    fn start(&self) {
+        println!("Wash::start");
+    }
+}
+
+use std::fmt::Debug;
+
+fn my_print<T: Debug>(x: T) {
+    // my_print函数引入了一个泛型参数T，所以它的参数不是一个具体类型，而是一组类型
+    // 要求这个T类型实现Debug这个trait
+    println!("The value is {:?}.", x);
+}
+
+fn my_print2<T>(x: T)
+where
+    T: Debug,
+{
+    println!("The value is {:?}.", x);
+}
+
 fn main() {
     let c = Circle { radius: 2_f64 };
     println!("The area is {}", c.area1());
@@ -68,4 +115,21 @@ fn main() {
 
     let x = T(42);
     T::func(&x);
+
+    let x: i32 = 10.double();
+    println!("{}", x);
+
+    let me = Chef;
+    <dyn Cook>::start(&me);
+    <Chef as Wash>::start(&me);
+
+    my_print("China"); // 凡是满足Debug约束的类型都可以是这个函数的参数
+    my_print(41_i32);
+    my_print(true);
+    my_print(['a', 'b', 'c']);
+
+    my_print2("China");
+    my_print2(41_i32);
+    my_print2(true);
+    my_print2(['a', 'b', 'c']);
 }
