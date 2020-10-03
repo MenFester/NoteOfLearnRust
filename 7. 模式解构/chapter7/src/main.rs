b2947e2b0f600bb4cb4959911abc1402a6d50340
MenFester <13500177552@qq.com>
2020-10-03 20:04:24 +0800

    2020年10月03日 20:04

--- 7. 模式解构/chapter7/src/main.rs
+++ 7. 模式解构/chapter7/src/main.rs
@@ -1,3 +1,22 @@
+struct T1 (i32, char);
+
+struct T2 {
+    item1: T1,
+    item2: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tuple = (1_i32, false, 3f32);
+    let (head, center, tail) = tuple;
+    println!("{} {} {}", head, center, tail);
+
+    let x = T2 {
+        item1: T1 (0, 'A'),
+        item2: false,
+    };
+    let T2 {
+        item1: T1 (value1, value2),
+        item2: value3,
+    } = x ;
+    println!("{} {} {}", value1, value2, value3);
 }
---
+++ 7. 模式解构/note.md
@@ -0,0 +1,15 @@
+# 学习笔记
+
+## 7.1 简介
+
+* 模式解构“Pattern Destructure”是Rust中一个重要且实用设计。Destructure意思是把原来的结构肢解为单独的、局部的、原始的部分
+* ` let (head, center, tail) = tuple `赋值号左边的内容就是“模式”，赋值号右边的内容就是需要被“解构”的内容
+* Rust中模式解构原则是：构造和解构遵循类似的语法
+* Rust的“模式解构”功能不仅出现let语句中，还可以出现在match、if-let、while-let、函数调用、闭包调用等情景中
+* match具有功能最强大的模式匹配
+
+## 7.2 match
+
+## 7.3 if-let和while-let
+
+## 7.4 函数和闭包参数做模式解构
